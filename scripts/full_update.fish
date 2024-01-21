f5b77de55b3bc2279c370cefcacf528a683cb7d3
Ian Nicolae <88843406+Vexiona@users.noreply.github.com>
2024-01-21 16:27:18 +0200

    create testing repo and edit scripts to add support

--- scripts/full_update.fish
+++ scripts/full_update.fish
@@ -1,22 +1,22 @@
 # Clean up the repo, re-create the DB and the release
 # #1: arch
 if test -z "$argv[1]"
-    set arch aarch64
+    set tag aarch64
 else
-    set arch $argv[1]
+    set tag $argv[1]
 end
-pushd pkgs/latest
+pushd pkgs/$tag/latest
 set files (readlink *)
 popd # pkgs/latest
 rm -rf releases
 mkdir releases
 pushd releases
 for file in $files
-    ln -sf (string replace '../' '../pkgs/' $file) (string split --right --max 1 --fields 2 '/' $file | string replace ':' '.')
+    ln -sf (string replace ../ ../pkgs/$tag/ $file) (string split --right --max 1 --fields 2 '/' $file | string replace ':' '.')
 end
-repo-add --verify --sign Vexiona.db.tar.zst *.pkg.tar.zst
-sudo rsync --recursive --verbose --copy-links --delete ./ /srv/http/repo/Vexiona/$arch &
-gh release delete --yes $arch
-gh release create $arch --title $arch --notes "Last full update at $(date)" --latest Vexiona.{db,files}{,.sig} *.pkg.tar.zst{,.sig}
+repo-add --verify --sign Vexiona-$tag.db.tar.zst *.pkg.tar.zst
+sudo rsync --recursive --verbose --copy-links --delete ./ /srv/http/repo/Vexiona/$tag &
+gh release delete --yes $tag
+gh release create $tag --title $tag --notes "Last full update at $(date)" --latest Vexiona-$tag.{db,files}{,.sig} *.pkg.tar.zst{,.sig}
 popd # releases
 wait # rsync
--- scripts/partial_update.fish
+++ scripts/partial_update.fish
@@ -1,11 +1,11 @@
 # Update packages on demand, re-create the DB and the release
 # #1: arch
 if test -z "$argv[1]"
-    set arch aarch64
+    set tag aarch64
 else
-    set arch $argv[1]
+    set tag $argv[1]
 end
-pushd pkgs/updated
+pushd pkgs/$tag/updated
 set pkgs *.pkg.tar.zst
 set sigs *.pkg.tar.zst.sig
 if test (count $pkgs) -eq 0
@@ -18,33 +18,33 @@ set files (readlink $pkgs $sigs)
 popd # pkgs/updated
 pushd releases
 for file in $files
-    ln -sf (string replace '../' '../pkgs/' $file) (string split --right --max 1 --fields 2 '/' $file | string replace ':' '.')
+    ln -sf (string replace ../ ../pkgs/$tag/ $file) (string split --right --max 1 --fields 2 '/' $file | string replace ':' '.')
 end
-repo-add --verify --sign Vexiona.db.tar.zst (string replace ':' '.' $pkgs) &
+repo-add --verify --sign Vexiona-$tag.db.tar.zst (string replace ':' '.' $pkgs) &
 for file in *.pkg.tar*
     if test ! -f $file
         rm -f $file
-        gh release delete-asset $arch $file --yes
+        gh release delete-asset $tag $file --yes
     end
 end
 wait
-sudo rsync --recursive --verbose --copy-links --delete ./ /srv/http/repo/Vexiona/$arch &
+sudo rsync --recursive --verbose --copy-links --delete ./ /srv/http/repo/Vexiona/$tag &
 set temp_assets (mktemp)
-gh release view $arch | sed -n 's/^asset:	\(.\+\)$/\1/p' > $temp_assets
+gh release view $tag | sed -n 's/^asset:	\(.\+\)$/\1/p' > $temp_assets
 set gh_files (string split --right --max 1 --fields 2 '/' $files | string replace ':' '.')
-set --append gh_files Vexiona.{db,files}{,.sig}
+set --append gh_files Vexiona-$tag.{db,files}{,.sig}
 for file in $gh_files
     if grep "^$file\$" $temp_assets
-        gh release delete-asset $arch $file --yes
+        gh release delete-asset $tag $file --yes
     end
 end
 rm -f $temp_assets
-gh release upload $arch $gh_files
+gh release upload $tag $gh_files
 popd # releases
 # Update the release note
-set full_note (gh release view $arch --json body | sed -n 's/.\+"\(Last full update at .\+\)".\+/\1/p' | string split --max 1 --fields 1 '\\')
+set full_note (gh release view $tag --json body | sed -n 's/.\+"\(Last full update at .\+\)".\+/\1/p' | string split --max 1 --fields 1 '\\')
 if test -n "$full_note"
-    gh release edit $arch --notes "$full_note
+    gh release edit $tag --notes "$full_note
     
 Last partial update at $(date)"
 end
